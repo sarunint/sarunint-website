e0012d39e16c392fcd10d7e0cac325cdfa12a615
Sarun Intaralawan <sarun.intaralawan@gmail.com>
2015-04-28 21:22:21 +0700

    Add core module

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,3 +1,11 @@
 //= require angular
 //= require jquery
 //= require bootstrap-sass
+/// <reference path="../typings/tsd.d.ts" />
+var sarunint_website;
+(function (sarunint_website) {
+    angular
+        .module('sarunint-website', []);
+})(sarunint_website || (sarunint_website = {}));
+/// <reference path="../typings/tsd.d.ts" />
+/// <reference path="app.ts" />
---
+++ source/javascripts/app.ts
@@ -0,0 +1,9 @@
+//= require angular
+//= require jquery
+//= require bootstrap-sass
+/// <reference path="../typings/tsd.d.ts" />
+
+module sarunint_website {
+    angular
+        .module('sarunint-website', []);
+}
