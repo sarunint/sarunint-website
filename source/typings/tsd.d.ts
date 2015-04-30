4238ca6765f3aa10bd146e5228b69fc766227b97
Sarun Intaralawan <sarun.intaralawan@gmail.com>
2015-04-30 20:00:36 +0700

    Add angular-resource to tsd.d.ts

--- source/typings/tsd.d.ts
+++ source/typings/tsd.d.ts
@@ -1,3 +1,4 @@
 /// <reference path="bootstrap/bootstrap.d.ts" />
 /// <reference path="jquery/jquery.d.ts" />
 /// <reference path="angularjs/angular.d.ts" />
+/// <reference path="angularjs/angular-resource.d.ts" />
